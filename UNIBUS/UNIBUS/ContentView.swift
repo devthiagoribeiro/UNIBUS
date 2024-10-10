d295ad32913e1c749c72a11508ed68b887729f59
lsaacemanuel <isaacdesa_nascimento@hotmail.com>
2024-10-10 15:36:55 -0300

    Update ContentView.swift

--- UNIBUS/UNIBUS/ContentView.swift
+++ UNIBUS/UNIBUS/ContentView.swift
@@ -6,39 +6,91 @@
 //
 
 import SwiftUI
+import BottomSheet
 
 struct ContentView: View {
+    @State var bottomSheetPosition: BottomSheetPosition = .absolute(180)
     @State var isPresenting = false
     @State var tabSelection = 0
     var body: some View {
         ZStack{
             TabView{
                 mapa()
+                    .bottomSheet(
+                        bottomSheetPosition: $bottomSheetPosition,
+                        switchablePositions: [.absolute(150),
+                                              .absolute(325), .relative(0.95)],
+                        headerContent: {
+                            
+                        },
+                        mainContent: {
+                            ScrollView{
+                                HStack{
+                                    VStack{
+                                        HStack{
+                                            ZStack{
+                                                Circle()
+                                                    .frame(width: 70, height: 70)
+                                                    .foregroundColor(.white)
+                                                Image(systemName: "bus.fill")
+                                                    .resizable()
+                                                    .foregroundColor(.black)
+                                                    .frame(width: 30, height: 30)
+                                            }
+                                            
+                                            VStack{                                            
+                                                   HStack{
+                                                    Text("Ônibus X")
+                                                    .font(.headline)
+                                                Spacer()
+                                            }
+                                                
+                                                HStack{
+                                                    Text("Avenida Monsenhor")
+                                                    Spacer()
+                                                }
+                                            }
+                                            
+                                            Spacer()
+                                            
+                                        }
+                                    }
+                                    Spacer()
+                                }
+                                .padding(.leading)
+                                Spacer()
+                            }
+                        }
+                    )
+                
                     .tabItem{
                         Label("Mapa", systemImage: "map.fill")
                     }
+                    .toolbarBackground(.visible, for: .tabBar)
                 rotas()
                     .tabItem{
-                        Label("Rotas", systemImage: "bus.fill")
+                    Label("Rotas", systemImage: "bus.fill")
                     }
                 Itinerario()
-                    .tabItem{
-                        Label("Itinerarios", systemImage: "clock.badge")
+                    .tabItem{ 
+                    Label("Itinerarios", systemImage: "clock.badge")
                     }
-                alertas()
-                    .tabItem{
-                        Label("Alertas", systemImage: "exclamationmark.brakesignal")
+               alertas()
+                   .tabItem{
+                    Label("Alertas", systemImage: "exclamationmark.brakesignal")
                     }
                 preferencias()
-                    .tabItem{
-                        Label("Preferencias", systemImage: "person.fill.questionmark")
+                    .tabItem{                                        
+                    Label("Preferencias", systemImage: "person.fill.questionmark")
                     }
             }
             .animation(.spring(), value: tabSelection)
         }
     }
 }
-#Preview {
-    ContentView()
-}
+                #Preview {
+                    ContentView()
+                }
+            
+
 
