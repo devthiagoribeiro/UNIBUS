68f27b23adcaca38328a53c66bdbb30af68b1fd9
João Emanuel <140533662+joaoeman@users.noreply.github.com>
2024-10-14 14:31:25 -0300

    Update preferencias.swift

--- UNIBUS/UNIBUS/preferencias.swift
+++ UNIBUS/UNIBUS/preferencias.swift
@@ -6,53 +6,55 @@
 //
 
 import SwiftUI
-import BottomSheet
+
 struct preferencias: View {
     @StateObject var viewModel = ViewModel()
     @State var selection: unibus?
     @State var selection1: unibus?
     @State var selection2: unibus?
     @State var selection3: unibus?
+    
     var body: some View {
         ZStack{
             Color.cor.ignoresSafeArea()
             VStack(){
                 Text("")
                 Spacer()
-                HStack{
                     Text("Preferências:")
                         .font(.title).padding()
-                }
-                Text("Onibus 1").bold()
+                        .padding(.horizontal, -160).bold()
+              
+                Text("Ônibus 1").bold()
+                        .padding(.horizontal, -140)
                 
                 Picker("Select a paint color", selection: $selection) {
                     ForEach(viewModel.chars, id: \.self) { p in
                         Text(p.nome!)
-                    }
-                   
-                    
+                            Spacer()
+                        }
                 }.padding(.horizontal,90).padding(.vertical,9)
                 .accentColor(.jo)
                 .background(Color.gray).cornerRadius(20.0).frame(width: 890)
                 .cornerRadius(12.0)
                 
-                Text("Onibus 2").bold()
+                Text("Ônibus 2").bold()
+                    .padding(.horizontal, -140)
                 
                 Picker("Select a paint color", selection: $selection1) {
                     ForEach(viewModel.chars, id: \.self) { p in
-                        
                         Text(p.nome!)
+                            
                     }
                 }.padding(.horizontal,90).padding(.vertical,9)
                 .accentColor(.jo)
                 .background(Color.gray).cornerRadius(20.0).frame(width: 890)
                 .cornerRadius(12.0)
                 
-                Text("Onibus 3").bold()
+                Text("Ônibus 3").bold()
+                    .padding(.horizontal, -140)
                 
                 Picker("Select a paint color", selection: $selection2) {
                     ForEach(viewModel.chars, id: \.self) { p in
-                        
                         Text(p.nome!)
                     }
                 }.padding(.horizontal,90).padding(.vertical,9)
@@ -60,8 +62,9 @@ struct preferencias: View {
                 .background(Color.gray).cornerRadius(20.0).frame(width: 890)
                 .cornerRadius(12.0)
                 
-                Text("Onibus 4").bold()
-                    
+                Text("Ônibus 4").bold()
+                    .padding(.horizontal, -140)
+                
                 Picker("Select a paint color", selection: $selection3) {
                     ForEach(viewModel.chars, id: \.self) { p in
                         
