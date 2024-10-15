2b27e80ce3fa2a7c6759dac2533ec8ba2203fdc5
João Emanuel <140533662+joaoeman@users.noreply.github.com>
2024-10-15 13:32:13 -0300

    Update api.swift

--- UNIBUS/UNIBUS/api.swift
+++ UNIBUS/UNIBUS/api.swift
@@ -11,11 +11,19 @@ struct coordenadas: Codable, Hashable{
     var latitude: Double?
     var longitude: Double?
 }
-    
+struct paradas: Codable,Hashable{
+    var lat: Double?
+    var long: Double?
+}
 struct unibus: Codable, Hashable{
     var _id: String
     var _rev: String
     var nome: String?
     var letra: String?
     var rota: [coordenadas]
+    var parada: [paradas]
+    var LA: Double?
+    var LO: Double?
+    var PositionX: Int?
+    var PositionY: Int?
 }
