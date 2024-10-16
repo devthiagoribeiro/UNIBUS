468ea91597c57ad28cff7dd2dc65df272750ec95
João Emanuel <140533662+joaoeman@users.noreply.github.com>
2024-10-16 15:12:58 -0300

    Update api.swift

--- UNIBUS/UNIBUS/api.swift
+++ UNIBUS/UNIBUS/api.swift
@@ -19,14 +19,43 @@ struct unibus: Codable, Hashable{
     var _id: String
     var _rev: String
     var nome: String
-    var letra: String
+    var letra: String?
     var rota: [coordenadas]
     var parada: [paradas]
-    var partida:String?
-    var chegada:String?
     var LA: Double?
     var LO: Double?
     var PositionX: Int?
     var PositionY: Int?
     var val: Bool?
+    var partida: String?
+    var chegada: String?
+    
+}
+
+struct unibusPref: Codable, Hashable{
+    var _id: String?
+    var _rev: String?
+    var ids: [String]
+    var date: Date?
+}
+
+
+extension Date {
+    struct Formatter {
+        static let iso8601: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.calendar = Calendar(identifier: .iso8601)
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+            formatter.timeZone = TimeZone(secondsFromGMT: 0)
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
+            return formatter
+        }()
+    }
+    var iso8601: String { return Formatter.iso8601.string(from: self) }
+}
+
+extension String {
+    var dateFromISO8601: Date? {
+        return Date.Formatter.iso8601.date(from: self)
+    }
 }
