a7edd49db97ed3ffefbf096be2606f2f0f6f80b9
João Emanuel <140533662+joaoeman@users.noreply.github.com>
2024-10-16 15:11:57 -0300

    Update preferencias.swift

--- UNIBUS/UNIBUS/preferencias.swift
+++ UNIBUS/UNIBUS/preferencias.swift
@@ -6,110 +6,181 @@
 //
 
 
+//
+//  preferancias.swift
+//  UNIBUS
+//
+//  Created by Turma01-27 on 10/10/24.
+//
+
 import SwiftUI
 
 struct preferencias: View {
     @StateObject var viewModel = ViewModel()
-    @State var selection: unibus?
-    @State var selection1: unibus?
-    @State var selection2: unibus?
-    @State var selection3: unibus?
+    
+    @State var selection: String = "Ônibus I"
+    @State var selection1: String = "Ônibus I"
+    @State var selection2: String = "Ônibus I"
+    @State var selection3: String = "Ônibus I"
+    //    @State var boolAux = false
+    
     
     var body: some View {
         ZStack{
             Color.cor.ignoresSafeArea()
-            VStack(){
-                Text("")
-                Spacer()
-                    Text("Preferências:")
+            
+            VStack{
+                
+                Text("Preferências:")
                     .foregroundStyle(.jo)
-                        .font(.title).padding()
-                        .padding(.horizontal, -160).bold()
-              
-                Text("Ônibus 1").bold().foregroundStyle(.jo)
-                        .padding(.horizontal, -140)
+                    .font(.title).padding()
+                  //  .padding(.horizontal, -160).bold()
+                
+                Text("ônibus 1").bold().foregroundStyle(.jo)
+                   // .padding(.horizontal, -140)
+                
                 
                 Picker("Select a paint color", selection: $selection) {
-                    ForEach(viewModel.chars, id: \.self) { p in
-                        Text(p.nome!)
-                            Spacer()
-                        }
+                    ForEach(viewModel.chars, id: \.nome) { p in
+                        Text(p.nome)
+                    }
                 }.padding(.horizontal,90).padding(.vertical,9)
-                .accentColor(.jo)
-                .background(Color.gray).cornerRadius(20.0).frame(width: 890)
-                .cornerRadius(12.0)
+                    .accentColor(.jo)
+                    .background(Color.gray).cornerRadius(20.0)
+                    //.frame(width: 890)
+                    .cornerRadius(12.0)
                 
                 Text("Ônibus 2").bold().foregroundStyle(.jo)
-                    .padding(.horizontal, -140)
+                   // .padding(.horizontal, -140)
                 
                 Picker("Select a paint color", selection: $selection1) {
-                    ForEach(viewModel.chars, id: \.self) { p in
-                        Text(p.nome!)
-                            
+                    ForEach(viewModel.chars, id: \.nome) { p in
+                        Text(p.nome)
+                        
                     }
                 }.padding(.horizontal,90).padding(.vertical,9)
-                .accentColor(.jo)
-                .background(Color.gray).cornerRadius(20.0).frame(width: 890)
-                .cornerRadius(12.0)
+                    .accentColor(.jo)
+                    .background(Color.gray).cornerRadius(20.0)
+                    //.frame(width: 890)
+                    .cornerRadius(12.0)
                 
                 Text("Ônibus 3").bold().foregroundStyle(.jo)
-                    .padding(.horizontal, -140)
+                   // .padding(.horizontal, -140)
                 
                 Picker("Select a paint color", selection: $selection2) {
-                    ForEach(viewModel.chars, id: \.self) { p in
-                        Text(p.nome!)
+                    ForEach(viewModel.chars, id: \.nome) { p in
+                        Text(p.nome)
                     }
                 }.padding(.horizontal,90).padding(.vertical,9)
-                .accentColor(.jo)
-                .background(Color.gray).cornerRadius(20.0).frame(width: 890)
-                .cornerRadius(12.0)
+                    .accentColor(.jo)
+                    .background(Color.gray).cornerRadius(20.0)
+                    //.frame(width: 890)
+                    .cornerRadius(12.0)
+                
                 
                 Text("Ônibus 4").bold().foregroundStyle(.jo)
-                    .padding(.horizontal, -140)
+                   // .padding(.horizontal, -140)
                 
                 Picker("Select a paint color", selection: $selection3) {
-                    ForEach(viewModel.chars, id: \.self) { p in
-                        
-                        Text(p.nome!)
+                    ForEach(viewModel.chars, id: \.nome) { p in
+                        Text(p.nome)
                     }
                 }.padding(.horizontal,90).padding(.vertical,9)
-                .pickerStyle(.menu)
-                .accentColor(.jo)
-                .background(Color.gray).cornerRadius(20.0).frame(width: 890)
-                .cornerRadius(12.0)
-            
+                    .pickerStyle(.menu)
+                    .accentColor(.jo)
+                    .background(Color.gray).cornerRadius(20.0)
+                    //.frame(width: 890)
+                    .cornerRadius(12.0)
                 
-                Spacer()
-                Spacer()
+//                ForEach(viewModel.chars, id: \.self){ p in
+//                    if(selection == p.nome){
+//                        
+//                    }
+//                }
+//                Spacer()
+//                Spacer()
+                
+                
                 Button("Salvar"){
-                    if(global.buli == true){
-                        global.selections?.removeAll()
-                        global.selections?.append(selection!)
-                        global.selections?.append(selection1!)
-                        global.selections?.append(selection2!)
-                        global.selections?.append(selection3!)
+                    
+                    
+                    // viewModel.removeAllPrefs(arr: viewModel.pref)
+                    
+                    //boolAux.toggle()
+                    //viewModel.post(u: _id: id, _rev: rev, val: boolAux)
+                    let bus1 =  viewModel.chars.filter{
+                        $0.nome == selection
                     }
-                    else{
-                        global.selections?.append(selection!)
-                        global.selections?.append(selection1!)
-                        global.selections?.append(selection2!)
-                        global.selections?.append(selection3!)
-                        global.buli = true
+                    let bus2 =  viewModel.chars.filter{
+                        $0.nome == selection1
                     }
+                    let bus3 =  viewModel.chars.filter{
+                        $0.nome == selection2
+                    }
+                    let bus4 =  viewModel.chars.filter{
+                        $0.nome == selection3
+                    }
+                    
+                    
+                    var arrPref : [String] = []
+                    arrPref.append(bus1.first!._id)
+                    arrPref.append(bus2.first!._id)
+                    arrPref.append(bus3.first!._id)
+                    arrPref.append(bus4.first!._id)
+                    
+                    
+                    
+                    viewModel.post(p: unibusPref(ids: arrPref, date: Date().iso8601.dateFromISO8601))
+                    
+                    
+                    viewModel.fetch()
+                    
+                    
+                    
+                    
+                    
+                    //                    if(globalStruct.buli == true){
+                    //                        globalStruct.selections?.removeAll()
+                    //                        globalStruct.selections?.append(selection!)
+                    //                        globalStruct.selections?.append(selection1!)
+                    //                        globalStruct.selections?.append(selection2!)
+                    //                        globalStruct.selections?.append(selection3!)
+                    //                    }
+                    //                    else{
+                    //                        globalStruct.selections?.append(selection!)
+                    //                        globalStruct.selections?.append(selection1!)
+                    //                        globalStruct.selections?.append(selection2!)
+                    //                        globalStruct.selections?.append(selection3!)
+                    //                        globalStruct.buli = true
+                    //                    }
+                    //                    print(globalStruct.buli)
+                    
+                    //foreach pra percorrer a API
+                    //condição: if o Bool for diferente de false
+                    //segundo forEach com as
                 }
-                .frame(height: 40)
-                .frame(width: 280)
-                .background(Color.white)
-                .foregroundColor(.black)
-                .cornerRadius(15.0)
-                Spacer()
+                
+                
+//                ScrollView{
+//                    VStack{
+//                        ForEach(viewModel.chars, id: \.self){ a in
+//                            Text(a.letra!)
+//                        }
+//                    }
+//                }
             }
-            .onAppear(){
-                viewModel.fetch()
-            }
+
+            
+
+        }
+        .onAppear(){
+            viewModel.fetch()
+            //print(globalStruct.buli)
         }
     }
 }
+
+
 #Preview {
     preferencias()
 }
