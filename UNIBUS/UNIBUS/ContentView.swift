0f5acddc1936945d792ade46b924caef7b008333
lsaacemanuel <isaacdesa_nascimento@hotmail.com>
2024-10-16 15:41:31 -0300

    Update ContentView.swift

--- UNIBUS/UNIBUS/ContentView.swift
+++ UNIBUS/UNIBUS/ContentView.swift
@@ -7,15 +7,51 @@
 
 import SwiftUI
 import BottomSheet
+import MapKit
+
 
 struct ContentView: View {
-    @State var bottomSheetPosition: BottomSheetPosition = .absolute(180)
+    @State var bottomSheetPosition: BottomSheetPosition = .absolute(150)
     @State var isPresenting = false
     @State var tabSelection = 0
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: -9.413454, longitude: -40.513541),
+            span: MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015)))
+    @State var positionX = 0
+    @State var positionY = 0
+    @StateObject var viewModel = ViewModel()
     var body: some View {
         ZStack{
             TabView{
-                mapa()
+                ZStack{
+                    Map(position: $position)
+                    {}
+                    ForEach(viewModel.chars, id: \.self){
+                        index in
+                        Map(position: $position)
+                        {
+                            Annotation("", coordinate: CLLocationCoordinate2D(latitude: index.LA!, longitude: index.LO!))
+                            {
+                                ZStack {
+                                    Circle()
+                                        .fill(.white)
+                                        .frame(width: 40, height: 40)
+                                    Text(index.letra!)
+                                        .font(.title)
+                                        .foregroundColor(Color.gray)
+                                }
+                                .offset(x: CGFloat(positionX), y: CGFloat(positionY))
+                                .animation(Animation.easeInOut(duration: 4).repeatForever(autoreverses: true).speed(0.1))
+                                .onAppear(){
+                                    positionX+=index.PositionX!
+                                    positionY-=index.PositionY!
+                                }
+                            }
+                        }
+                    }
+
+                }
                     .bottomSheet(
                         bottomSheetPosition: $bottomSheetPosition,
                         switchablePositions: [.absolute(150),
@@ -25,72 +61,78 @@ struct ContentView: View {
                         },
                         mainContent: {
                             ScrollView{
-                                HStack{
-                                    VStack{
-                                        HStack{
-                                            ZStack{
-                                                Circle()
-                                                    .frame(width: 70, height: 70)
-                                                    .foregroundColor(.white)
-                                                Image(systemName: "bus.fill")
-                                                    .resizable()
-                                                    .foregroundColor(.black)
-                                                    .frame(width: 30, height: 30)
-                                            }
-                                            
-                                            VStack{                                            
-                                                   HStack{
-                                                    Text("Ônibus X")
-                                                    .font(.headline)
-                                                Spacer()
-                                            }
+                                ForEach(viewModel.chars, id: \.self){
+                                    index in
+                                    HStack{
+                                        VStack{
+                                            HStack{
+                                                ZStack{
+                                                    Circle()
+                                                        .frame(width: 70, height: 70)
+                                                        .foregroundColor(.white)
+                                                    Image(systemName: "bus.fill")
+                                                        .resizable()
+                                                        .foregroundColor(.black)
+                                                        .frame(width: 30, height: 30)
+                                                }
                                                 
-                                                HStack{
-                                                    Text("Avenida Monsenhor")
+                                                VStack{                                             HStack{                              Text(index.nome)
+                                                        .font(.headline)
                                                     Spacer()
                                                 }
+                                                    
+                                                    HStack{
+                                                        Text("Avenida Monsenhor")
+                                                        Spacer()
+                                                    }
+                                                }
+                                                
+                                                Spacer()
+                                                
                                             }
-                                            
-                                            Spacer()
-                                            
                                         }
+                                        Spacer()
                                     }
+                                    .onTapGesture {
+                                        position=MapCameraPosition.region(MKCoordinateRegion(
+                                            center: CLLocationCoordinate2D(latitude: index.LA!, longitude: index.LO!),
+                                            span: MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015)))
+                                    }
+                                    .padding(.leading)
                                     Spacer()
                                 }
-                                .padding(.leading)
-                                Spacer()
                             }
-                        }
-                    )
-                
+                        })
                     .tabItem{
                         Label("Mapa", systemImage: "map.fill")
                     }
                     .toolbarBackground(.visible, for: .tabBar)
                 rotas()
-                    .tabItem{
-                    Label("Rotas", systemImage: "bus.fill")
-                    }
-                Itinerario()
-                    .tabItem{ 
-                    Label("Itinerarios", systemImage: "clock.badge")
-                    }
-               alertas()
-                   .tabItem{
-                    Label("Alertas", systemImage: "exclamationmark.brakesignal")
-                    }
-                preferencias()
-                    .tabItem{                                        
-                    Label("Preferencias", systemImage: "person.fill.questionmark")
-                    }
+                                    .tabItem{
+                                        Label("Rotas", systemImage: "bus.fill")
+                                    }
+                                Itinerario()
+                                    .tabItem{
+                                        Label("Itinerarios", systemImage: "clock.badge")
+                                    }
+                                alertas()
+                                    .tabItem{
+                                        Label("Alertas", systemImage: "exclamationmark.brakesignal")
+                                    }
+                                preferencias()
+                                    .tabItem{
+                                        Label("Preferencias", systemImage: "person.fill.questionmark")
+                                    }
             }
-            .animation(.spring(), value: tabSelection)
-        }
-    }
-}
+                            .animation(.spring(), value: tabSelection)
+                        }
+                            .onAppear(){
+                                viewModel.fetch()
+                    }
+                }
+                        }
+                
                 #Preview {
                     ContentView()
                 }
             
-
-
